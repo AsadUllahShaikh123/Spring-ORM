df1ae21201612a39f15d7f154f6945b4c3e58d89
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-05 04:28:45 +0500

    two ways to redirect the website

--- src/main/java/com/spring_orm/controllers/MyController.java
+++ src/main/java/com/spring_orm/controllers/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.spring_orm.model.Student;
 import com.spring_orm.service.StudentService;
@@ -18,24 +19,39 @@ public class MyController {
 	private StudentService service;
 
 	@RequestMapping("/register")
-	public String register() {
+	public String register(@ModelAttribute Student student) {
 
-		return "register";
+		service.saveUser(student);
+
+		System.out.println("Name : " + student.getName() + "Email : " + student.getEmail());
+		return "redirect:/register";
 	}
 
 	@RequestMapping("/login")
-	public String login() {
-
+	public String login(@ModelAttribute Student student) {
+		
+		
 		return "login";
 	}
 
 	@RequestMapping("/success")
-	public String success(@ModelAttribute Student student) {
-		service.saveUser(student);
-
-		System.out.println("Name : " + student.getName() + "Email : " + student.getEmail());
+	public String success() {
+		
 
 		return "redirect:/login";
+	}
+	
+	@RequestMapping("/google")
+	public String getGoogleSite() {
+		
+		return "redirect:https://www.google.com/";
+	}
+	@RequestMapping("/youtube")
+	public RedirectView getYoutubeSite() {
+		RedirectView redirect = new RedirectView();
+		
+		redirect.setUrl("https://www.youtube.com/");
+		return redirect;
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2 style="text-align: center">Want to visit other sites ? </h2>
+<div style="text-align: center">
+	<a href="google">Go to Google</a><a href="youtube"> Go to Youtube </a>
+</div>
 </body>
 </html>
